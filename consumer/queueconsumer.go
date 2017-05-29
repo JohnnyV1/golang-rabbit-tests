e280647ff351e1bda9eb6c823a4e1616c97897e8
JohnnyV1 <vinu_john@hotmail.com>
2017-05-29 14:53:33 -0700

    Changes to accomadate queueservice.

--- consumer/queueconsumer.go
+++ consumer/queueconsumer.go
@@ -7,7 +7,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"runtime"
 	"strconv"
 	"sync"
@@ -15,16 +14,14 @@ import (
 
 	"strings"
 
-	"github.com/streadway/amqp"
 	"github.com/vinujohn/qsvc"
 )
 
-var counter int         //keeps track of messages processed
-var starttime time.Time //time before goroutines started
-var mu sync.Mutex       //sync between shared goroutine state
+var counter int   //keeps track of messages processed
+var mu sync.Mutex //sync between shared goroutine state
 
 const (
-	numberOfMessages        int = 1000 //number of messages to publish to the queue
+	numberOfMessages        int = 100  //number of messages to publish to the queue
 	inputToLargestPrimeFunc     = 1000 //input to the work function which each goroutine calls
 	numberOfProcessors          = -1   //number of processors to use for this program. -1 signifies use default which should be cores on the machine.
 )
@@ -33,71 +30,38 @@ type processor struct {
 	subscriberName string
 }
 
-func (p processor) Process(b []byte) {
-	log.Printf("Received a message: %s for subscriber %s\n", b, p.subscriberName)
+func (p processor) Process(message []byte) {
+	log.Printf("Processing message: %s\n", message)
 	value, _ := largestPrimeFactor(inputToLargestPrimeFunc)
 	log.Printf("Largest prime %d\n", value)
-	//exit()
+	zero := 0
+	if string(message) == "what" {
+		//panic("Not expecting this value.")
+		log.Printf("%d", 5/zero)
+	}
 }
 
 func main() {
 
 	runtime.GOMAXPROCS(numberOfProcessors)
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
+	svc := qsvc.New("amqp://guest:guest@localhost:5672/")
 
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when usused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
+	starttime := time.Now()
 	for i := 0; i < numberOfMessages; i++ {
 		message := strings.Join([]string{"hello world ", strconv.FormatInt(int64(i), 10)}, "")
-		ch.Publish(
-			"",
-			q.Name,
-			false,
-			false,
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(message),
-			},
-		)
+		svc.Publish("hello", []byte(message))
 	}
+	log.Printf("Time Taken to publish %d: %d", numberOfMessages, getElapsedTime(starttime))
 
-	svc2 := qsvc.New("amqp://guest:guest@localhost:5672/")
+	svc.Subscribe("hello", processor{})
 
-	forever := make(chan bool)
-
-	go svc2.Subscribe("hello", processor{subscriberName: "sub2"})
-
-	go svc2.Subscribe("hello", processor{subscriberName: "sub3"})
-
-	<-forever
+	//go svc2.Subscribe("hello", processor{subscriberName: "sub3"})
 
 }
 
-func exit() {
-	mu.Lock()
-	counter++
-	if counter == numberOfMessages {
-		i := time.Since(starttime)
-		log.Printf("Time Taken: %d", (i.Nanoseconds() / int64(1000000)))
-		os.Exit(0)
-	}
-	mu.Unlock()
+func getElapsedTime(startTime time.Time) int64 {
+	return time.Since(startTime).Nanoseconds() / int64(1000000)
 }
 
 func failOnError(err error, msg string) {
